1a95df1cdeb753b41b37c8b35e6e1ed31505f566
livos <lvivancos2nd-dev@yahoo.fr>
2022-06-06 17:54:18 +0700

    single product - product images

--- src/components/PageHero.js
+++ src/components/PageHero.js
@@ -3,14 +3,14 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 const PageHero = ({ title, product }) => {
   return (
-    <wrapper>
+    <Wrapper>
       <div className="section-center">
         <h3>
           <Link to="/">Home</Link>/
           {product && <Link to="/products">Products</Link>}/{title}
         </h3>
       </div>
-    </wrapper>
+    </Wrapper>
   );
 };
 
